d66d2db279985410402da75ebe27279c9242a59d
Diogo Gomes <diogogomes079@gmail.com>
2016-08-15 17:48:45 -0300

    ajustando o whitenoise no projeto

--- animeviciodesu/settings.py
+++ animeviciodesu/settings.py
@@ -89,7 +89,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
@@ -107,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
